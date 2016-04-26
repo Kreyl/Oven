ab4d69688686b88592cd0973aa42bbbbeee415f0
Kreyl <laystoll@yandex.ru>
2016-04-26 04:31:06 +0300

    + Sch started

---
+++ Oven.mm
@@ -0,0 +1,62 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Oven" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1461432631326"><hook NAME="MapStyle" zoom="1.5">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="bubble">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" BACKGROUND_COLOR="#ccffcc" STYLE="bubble"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" BACKGROUND_COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" BACKGROUND_COLOR="#ffcccc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="&#x41c;&#x430;&#x442;&#x435;&#x440;&#x438;&#x430;&#x43b;&#x44b;" POSITION="right" ID="ID_98860821" CREATED="1461432672595" MODIFIED="1461432689776">
+<node TEXT="&#x422;&#x440;&#x443;&#x431;&#x430; 15&#x445;15&#x445;1.5" ID="ID_1364968856" CREATED="1461432691563" MODIFIED="1461432702784"/>
+<node TEXT="&#x422;&#x440;&#x443;&#x431;&#x430; 20&#x445;10&#x445;1.5" ID="ID_169910853" CREATED="1461432703931" MODIFIED="1461432726240"/>
+<node TEXT="&#x422;&#x440;&#x443;&#x431;&#x430; 10&#x445;10&#x445;1" ID="ID_225393602" CREATED="1461432783946" MODIFIED="1461432797984"/>
+<node TEXT="&#x422;&#x440;&#x443;&#x431;&#x430; 40&#x445;40" ID="ID_1760441409" CREATED="1461500949076" MODIFIED="1461500963482"/>
+<node TEXT="&#x423;&#x433;&#x43e;&#x43b; 12&#x445;12" ID="ID_363868229" CREATED="1461432798691" MODIFIED="1461433956666"/>
+<node TEXT="&#x423;&#x433;&#x43e;&#x43b; 10&#x445;10&#x445;1.5" ID="ID_302841182" CREATED="1461433994021" MODIFIED="1461434005100"/>
+<node TEXT="&#x41f;&#x43e;&#x43b;&#x43e;&#x441;&#x430; 2&#x445;15" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_1247546567" CREATED="1461433974917" MODIFIED="1461520063982"/>
+<node TEXT="&#x41f;&#x43e;&#x43b;&#x43e;&#x441;&#x430; 2&#x445;20" ID="ID_1860994254" CREATED="1461434137213" MODIFIED="1461434143851"/>
+<node TEXT="&#x41f;&#x43e;&#x43b;&#x43e;&#x441;&#x430; 2&#x445;25" ID="ID_1157816352" CREATED="1461434151183" MODIFIED="1461434161692"/>
+<node TEXT="&#x428;&#x432;&#x435;&#x43b;&#x43b;&#x435;&#x440; 10&#x445;15&#x445;10" ID="ID_356494177" CREATED="1461434026181" MODIFIED="1461434126957"/>
+<node TEXT="&#x428;&#x432;&#x435;&#x43b;&#x43b;&#x435;&#x440; 20&#x445;20&#x445;20&#x445;1.5" ID="ID_1608148778" CREATED="1461434398686" MODIFIED="1461434409603"/>
+<node TEXT="&#x428;&#x432;&#x435;&#x43b;&#x43b;&#x435;&#x440; 15&#x445;20&#x445;15&#x445;1.5" ID="ID_219607373" CREATED="1461434430623" MODIFIED="1461434446068"/>
+<node TEXT="&#x41f;&#x43e;&#x440;&#x43e;&#x433; 60&#x445;5&#x445;900" ID="ID_907729357" CREATED="1461434478888" MODIFIED="1461434492283"/>
+</node>
+</node>
+</map>
